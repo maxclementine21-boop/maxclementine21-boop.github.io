2d8f87c8ec880ead0504352c3784db18d755c434
Zichenghao <maxclementine21@gmail.com>
2025-10-31 11:00:06 +0000

    Fix title formatting in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Zicheng Hao's personal web ',
+  title: 'Zicheng Hao personal web ',
   description: 'Created with v0',
   generator: '',
 }

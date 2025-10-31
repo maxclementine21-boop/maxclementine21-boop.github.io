1e955f05fbd602e9c237ffa00efbda8d4804369f
Zichenghao <maxclementine21@gmail.com>
2025-10-31 10:58:05 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Zicheng Hao's personal web ',
   description: 'Created with v0',
-  generator: 'v0.app',
+  generator: '',
 }
 
 export default function RootLayout({
